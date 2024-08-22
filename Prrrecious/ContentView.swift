476f761ed38045dc59b2c12e426fed4992e7dd4f
Vladimir Kukushkin <vladimir.kukushkin@meetiqm.com>
2024-08-22 22:17:11 +0300

    add content padding

--- Prrrecious/ContentView.swift
+++ Prrrecious/ContentView.swift
@@ -24,6 +24,7 @@ struct ContentView: View {
                 }
                 .onDelete(perform: deleteItems)
             }
+            .padding()
             .toolbar {
                 #if os(iOS)
                     ToolbarItem(placement: .navigationBarTrailing) {
